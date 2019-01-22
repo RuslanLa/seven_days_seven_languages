a8c87bb94c728aab206b7acb61fc805b85499ac8
RuslanLatypov <ruslan.latypov@akvelon.com>
2019-01-22 09:01:47 +0300

    prolog day 1 musician and authors

---
+++ prolog/authors.pl
@@ -0,0 +1,12 @@
+authors(martin, clean_code).
+authors(martin, clean_architecture).
+authors(haksli, island).
+authors(bryson, short_history).
+
+#authors(martin, Book).
+
+musician(ginger_baker, drums, blues).
+musician(john_baldry, guitar, blues).
+musician(david_silveria, drums, rock).
+
+#musician(Name, guitar, Genre).
---
+++ prolog/map.pl
@@ -0,0 +1,16 @@
+different(red, green).
+different(green, red).
+different(blue, red).
+different(red, blue).
+different(green, blue).
+different(blue, green).
+
+coloring(Alabama, Mississippi, Georgia, Tennessee, Florida) :-
+different(Mississippi, Tennessee),
+different(Mississippi, Alabama),
+different(Alabama, Tennessee),
+different(Alabama, Mississippi),
+different(Alabama, Georgia),
+different(Alabama, Florida),
+different(Georgia, Florida),
+different(Georgia, Tennessee).
